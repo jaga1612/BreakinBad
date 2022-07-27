eabef526c9ddf87186920a516e8d1a8cb4236801
jaga1612 <jagadeesh.mech.022@gmail.com>
2022-07-27 16:11:51 +0530

    dropdown close update

--- src/components/CherectorList.js
+++ src/components/CherectorList.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import "../style.css"
 import { getCharectorList, isFetchCompleted } from "../action/index.js";
 import Listitem from "./listItem";
 import Dropdown from "./dropDown";
@@ -8,9 +7,9 @@ import Dropdown from "./dropDown";
 // import history from "../history.js";
 
 class CharectorList extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={currentPage:1,perPage:5}
+  constructor(props) {
+    super(props);
+    this.state = { currentPage: 1, perPage: 5, dropDownValue: [5, 10, 20] };
   }
 
   componentDidMount() {
@@ -18,71 +17,122 @@ class CharectorList extends React.Component {
     this.props.getCharectorList();
   }
 
-  setPerPage=(perPage)=>{
-    this.setState({perPage:perPage})
-  }
-
-  renderList () {
-  return <Listitem cherectorlist={this.props.cherectorlist.slice((this.state.perPage*this.state.currentPage)-this.state.perPage,(this.state.currentPage*this.state.perPage))}/>
-  }
-
-  pageNoChange(e,action,noOfPage){
-    e.preventDefault()
-    if(action==="P"){
-      if(this.state.currentPage===1){
-          return;
-        }else{
-          this.setState({currentPage:this.state.currentPage-1})
-    }
+  setPerPage = (perPage) => {
+    this.setState({ perPage: perPage });
   };
-    if(action==="N"){
-      if(this.state.currentPage===noOfPage){
-          return;
-        }else{
-          this.setState({currentPage:this.state.currentPage+1})
-    }  
+  renderList() {
+    return (
+      <Listitem
+        cherectorlist={this.props.cherectorlist.slice(
+          this.state.perPage * this.state.currentPage - this.state.perPage,
+          this.state.currentPage * this.state.perPage
+        )}
+      />
+    );
   }
-  
-}
-  
 
-  renderPageNo=()=>{ 
-
-    const noOfPage = Math.ceil(this.props.cherectorlist.length/this.state.perPage)
-    const pageNo = []
-        for(let i =0; i<noOfPage; i++){
-        pageNo.push(<li key={i} onClick={(e)=>{
-          e.preventDefault()
-          this.setState({currentPage:i+1})
-        }} className={`page-item ${this.state.currentPage===i+1?"active":null}`}><button className="page-link">{i+1}</button></li>)
+  pageNoChange(e, action, noOfPage) {
+    e.preventDefault();
+    if (action === "P") {
+      if (this.state.currentPage === 1) {
+        return;
+      } else {
+        this.setState({ currentPage: this.state.currentPage - 1 });
       }
-             
-    return (<div>
-    <ul className="pagination justify-content-end">
-      <li onClick={(e)=>{this.pageNoChange(e,"P",noOfPage)}} className="page-item"><button className="page-link">Previous</button></li>
-      {pageNo}
-      <li onClick={(e)=>{this.pageNoChange(e,"N",noOfPage)}} className="page-item"><button className="page-link">Next</button></li>
-    </ul>
-  </div>)
+    }
+    if (action === "N") {
+      if (this.state.currentPage === noOfPage) {
+        return;
+      } else {
+        this.setState({ currentPage: this.state.currentPage + 1 });
+      }
+    }
   }
+
+  renderPageNo = () => {
+    const noOfPage = Math.ceil(
+      this.props.cherectorlist.length / this.state.perPage
+    );
+    const pageNo = [];
+    for (let i = 0; i < noOfPage; i++) {
+      pageNo.push(
+        <li
+          key={i}
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({ currentPage: i + 1 });
+          }}
+          className={`page-item ${
+            this.state.currentPage === i + 1 ? "active" : null
+          }`}
+        >
+          <button className="page-link">{i + 1}</button>
+        </li>
+      );
+    }
+
+    return (
+      <div>
+        <ul className="pagination justify-content-end">
+          <li
+            onClick={(e) => {
+              this.pageNoChange(e, "P", noOfPage);
+            }}
+            className="page-item"
+          >
+            <button className="page-link">Previous</button>
+          </li>
+          {pageNo}
+          <li
+            onClick={(e) => {
+              this.pageNoChange(e, "N", noOfPage);
+            }}
+            className="page-item"
+          >
+            <button className="page-link">Next</button>
+          </li>
+        </ul>
+      </div>
+    );
+  };
 
   render() {
     // console.log(this.state)
-    return (<React.Fragment>
-      <div className="row">
-        <div className="col">{Math.ceil(this.props.cherectorlist.length/this.state.perPage)>=1&&this.props.isFetchComplete?this.renderPageNo():null}</div>
-        <div className="col">{Math.ceil(this.props.cherectorlist.length/this.state.perPage)>=1&&this.props.isFetchComplete?<Dropdown perPage={this.state.perPage} setPerPage={this.setPerPage}/>:null}</div>
-        
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col">
+            {Math.ceil(this.props.cherectorlist.length / this.state.perPage) >=
+              1 && this.props.isFetchComplete
+              ? this.renderPageNo()
+              : null}
+          </div>
+          <div className="col">
+            {Math.ceil(this.props.cherectorlist.length / this.state.perPage) >=
+              1 && this.props.isFetchComplete ? (
+              <Dropdown
+                perPage={this.state.perPage}
+                setPerPage={this.setPerPage}
+                dropDownValue={this.state.dropDownValue}
+              />
+            ) : null}
+          </div>
         </div>
-    <div className="list-group">{this.props.isFetchComplete?this.renderList():'Loading...'}</div>
-    </React.Fragment>
+        <div className="list-group">
+          {this.props.isFetchComplete ? this.renderList() : "Loading..."}
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-
 const mapStateToProps = (state) => {
-   return { cherectorlist: state.charector, isFetchComplete:state.isFetchCompleted };
+  return {
+    cherectorlist: state.charector,
+    isFetchComplete: state.isFetchCompleted,
+  };
 };
 
-export default connect(mapStateToProps, { getCharectorList, isFetchCompleted })(CharectorList);
+export default connect(mapStateToProps, { getCharectorList, isFetchCompleted })(
+  CharectorList
+);
--- src/components/dropDown.js
+++ src/components/dropDown.js
@@ -1,28 +1,56 @@
 import React, { useState } from "react";
 
 const Dropdown = function (props) {
-  const[show,setShow] = useState("")
-   return (
+  const [show, setShow] = useState("");
+
+  const root = document.querySelector("#root");
+
+  root.addEventListener("click", (e) => {
+    if (!e.target.classList.contains("dropdown-toggle")) {
+      setShow("");
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      setShow("");
+    }
+  });
+
+  const dropDownValue = () => {
+    return props.dropDownValue.map((val) => {
+      return (
+        <button
+          key={val}
+          onClick={() => {
+            setShow("");
+            props.setPerPage(val);
+          }}
+          className="dropdown-item"
+          type="button"
+        >
+          {val}
+        </button>
+      );
+    });
+  };
+
+  return (
     <div>
-<div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle" onClick={()=>{setShow(show?"":"show")}} type="button">
-    {props.perPage}
-  </button>
-  <ul className={`dropdown-menu ${show}`}>
-    <li><button onClick={()=>{
-      setShow("");
-      props.setPerPage(5);
-      }} className="dropdown-item" type="button">5</button></li>
-    <li><button onClick={()=>{
-      setShow("");
-      props.setPerPage(10);
-      }} className="dropdown-item" type="button">10</button></li>
-    <li><button onClick={()=>{
-      setShow("");
-      props.setPerPage(20);
-      }} className="dropdown-item" type="button">20</button></li>
-  </ul>
-</div>
+      <div className="dropdown">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          onClick={() => {
+            setShow(show ? "" : "show");
+          }}
+          type="button"
+        >
+          {props.perPage}
+        </button>
+        <ul className={`dropdown-menu ${show}`}>
+          <li>{dropDownValue()}</li>
+        </ul>
+      </div>
     </div>
   );
 };
